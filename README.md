2b4a0c131dd3b6d51aa456eeccb9a17e7ee2d337
Juergen <juergen.a2025@gmail.com>
2025-01-15 01:33:57 +0100

    Finalised color generator without stretch goals

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Node.js Random Color Generator
+
+## Display a color generator with or without user input visualising with colored box
+
+### Code blocks
+
+'''
+node index.js without arguments - random color generation
+
+'''
+
+node index.js with arguments 1-hue and 2-luminosity - colored color generation
+
+'''
+
+node index.js with arguments 1-hue and 2-luminosity - visualisation of user interaction with colored box
+
+'''
+
+Tasks
+
+[] figure out library availability
+
+[] figure out how to print hex-values
+
+[] igure out how to color hex-values
+
+[] figure out how to pass user input from terminal to color generator
+
+[] figure out how to code color box
+
+[] figure out how to print color box to console
---
+++ index.js
@@ -0,0 +1,33 @@
+// Enter arguments in the command line
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import argv from 'node:process';
+// Import module for coloring random colors
+import kuler from 'kuler';
+// Import module for random color generation
+import randomColor from 'randomcolor';
+
+// Extract random hex-code based on user input
+const color = randomColor({
+  luminosity: process.argv[3],
+  hue: process.argv[2],
+});
+
+// Coloring box elements
+const elHashColorLang = kuler('#'.repeat(31), color);
+const elHashColorKurz = kuler('#'.repeat(5), color);
+const elEmptyLang = kuler(' '.repeat(19), color);
+const elEmptyKurz = kuler(' '.repeat(5), color);
+const elHexColor = kuler(`${color}`, color);
+
+// Print box elements to console
+console.log(`
+  ${elHashColorLang}
+  ${elHashColorLang}
+  ${elHashColorLang}
+  ${elHashColorKurz} ${elEmptyLang} ${elHashColorKurz}
+  ${elHashColorKurz} ${elEmptyKurz} ${elHexColor} ${elEmptyKurz} ${elHashColorKurz}
+  ${elHashColorKurz} ${elEmptyLang} ${elHashColorKurz}
+  ${elHashColorLang}
+  ${elHashColorLang}
+  ${elHashColorLang}
+  `);
