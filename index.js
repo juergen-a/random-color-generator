4b9f534cb06264849c05eb8ddcbfad2b46b51cd7
Juergen <juergen.a2025@gmail.com>
2025-01-15 13:21:26 +0100

    Delete unused argv-import

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// Enter arguments in the command line
-
-import argv from 'node:process';
 // Import module for coloring random hex color strings
 import kuler from 'kuler';
 // Import module for random color generation
 import randomColor from 'randomcolor';
 
+// const varLum = argv[3];
+const test = process.argv;
+const hueVal = test[2];
+const luminosityVal = test[3];
+
 // Extract random hex-code based on user input
 const color = randomColor({
-  luminosity: process.argv[3],
-  hue: process.argv[2],
+  luminosity: luminosityVal,
+  hue: hueVal,
 });
 
 // Coloring box elements
@@ -17,7 +19,7 @@ const elHashColorLang = kuler('#'.repeat(31), color);
 const elHashColorKurz = kuler('#'.repeat(5), color);
 const elEmptyLang = kuler(' '.repeat(19), color);
 const elEmptyKurz = kuler(' '.repeat(5), color);
-const elHexColor = kuler(`${color}`, color);
+const elHexColor = kuler(color, color);
 
 // Print box elements to console
 console.log(`
